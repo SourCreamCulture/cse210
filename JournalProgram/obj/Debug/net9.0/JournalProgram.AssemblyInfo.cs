19ff30a10f951ae0385c8d99ec14f40a30a2887e
Dallin Bland <73759064+SourCreamCulture@users.noreply.github.com>
2025-05-14 14:36:37 -0600

    fix

--- JournalProgram/obj/Debug/net9.0/JournalProgram.AssemblyInfo.cs
+++ JournalProgram/obj/Debug/net9.0/JournalProgram.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JournalProgram")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc102dcbe5b7cfd0bc4c78447f5197251510d69b")]
 [assembly: System.Reflection.AssemblyProductAttribute("JournalProgram")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JournalProgram")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
